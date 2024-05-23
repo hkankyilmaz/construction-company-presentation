de982ee568415660246c8314bc908115e571fc2f
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-05-23 22:06:33 +0300

    Update SliderForm.jsx

--- src/components/slider/SliderForm.jsx
+++ src/components/slider/SliderForm.jsx
@@ -4,12 +4,12 @@ function SliderForm() {
   return (
     <form className="flex flex-col max-md:w-[310px] md:max-w-[600px] lg:absolute bg-white lg:bottom-[20%] lg:right-[7%] xl:right-[18%] space-y-3 md:space-y-5 p-4 md:p-20 rounded-2xl">
       <h2 className="text-lg md:text-2xl font-semibold">
-        SİZ DE YENİ DEĞER'E KATILIN!
+        SİZ DE YENİ DEĞER&apos;E KATILIN!
       </h2>
       <p className="text-gray-500 max-md:text-xs">
-        Proje hakkında daha fazla bilgi almak ve satın alma & ödeme seçenekleri
-        için bizimle iletişime geçebilirsiniz. İgilendiğiniz daire tipini
-        seçmeyi unutmayın. Size hemen dönüş yapacağız.
+        Proje hakkında daha fazla bilgi almak ve satın alma &amp; ödeme
+        seçenekleri için bizimle iletişime geçebilirsiniz. İgilendiğiniz daire
+        tipini seçmeyi unutmayın. Size hemen dönüş yapacağız.
       </p>
       <input
         className="max-md:text-xs border bg-inputInnerColor p-2 md:p-3outline-none ring-0"
@@ -28,7 +28,7 @@ function SliderForm() {
       </select>
       <div className="flex items-center">
         <input className="cursor-pointer" type="checkbox" />
-        <label className="text-gray-500 ml-2 max-md:text-xs" for="checkbox">
+        <label className="text-gray-500 ml-2 max-md:text-xs" htmlFor="checkbox">
           Kişisel verilerimin işlenmesine izin veriyorum.
         </label>
       </div>
