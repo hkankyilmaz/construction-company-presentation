bdf172d6d2314b91af8f946aa91d56a61d8ce7a8
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-05-26 14:27:38 +0300

    section-nine edit

--- src/components/sections/SectionNine.jsx
+++ src/components/sections/SectionNine.jsx
@@ -5,22 +5,28 @@ import { p } from "@/assets/photosUrl";
 
 function SectionNine() {
   return (
-    <section className="grid grid-cols-4 grid-rows-4 gap-1  [&i>img]:h-full my-[120px]">
-      <Card src={p.sectionNine.firstPhoto} />
-      <Card
-        src={p.sectionNine.secondPhoto}
-        tailwindcss={"col-span-2 row-span-2"}
-      />
-      <Card src={p.sectionNine.thirdPhoto} />
-      <Card src={p.sectionNine.fourthPhoto} />
-      <Card src={p.sectionNine.fifthPhoto} />
-      <Card src={p.sectionNine.sixthPhoto} />
-      <Card src={p.sectionNine.seventhPhoto} />
-      <Card src={p.sectionNine.eighthPhoto} />
-      <Card src={p.sectionNine.ninthPhoto} tailwindcss={"row-span-2"} />
-      <Card src={p.sectionNine.tenthPhoto} />
-      <Card src={p.sectionNine.eleventhPhoto} />
-      <Card src={p.sectionNine.twelfthPhoto} />
+    <section className="my-[120px] max-md:p-1">
+      <div className="flex flex-col justify-center items-center p-10 bg-[#072326] text-white">
+        <h3 className="text-xs mb-2">PRJOE GÖRSELLERİ</h3>
+        <h2 className="text-2xl md:text-5xl">Yeni Değer Konakları</h2>
+      </div>
+      <div className="grid grid-cols-2 py-1 md:grid-cols-4 md:grid-rows-4 gap-1">
+        <Card src={p.sectionNine.firstPhoto} />
+        <Card
+          src={p.sectionNine.secondPhoto}
+          tailwindcss={"md:col-span-2 md:row-span-2"}
+        />
+        <Card src={p.sectionNine.thirdPhoto} />
+        <Card src={p.sectionNine.fourthPhoto} />
+        <Card src={p.sectionNine.fifthPhoto} />
+        <Card src={p.sectionNine.sixthPhoto} />
+        <Card src={p.sectionNine.seventhPhoto} />
+        <Card src={p.sectionNine.eighthPhoto} />
+        <Card src={p.sectionNine.ninthPhoto} tailwindcss={"md:row-span-2"} />
+        <Card src={p.sectionNine.tenthPhoto} />
+        <Card src={p.sectionNine.eleventhPhoto} />
+        <Card src={p.sectionNine.twelfthPhoto} />
+      </div>
     </section>
   );
 }
